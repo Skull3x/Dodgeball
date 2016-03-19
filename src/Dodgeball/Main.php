743d07d678b1d488338a564e548747fa90332ed3
Skullex <jross3740@gmail.com>
2016-03-18 23:28:38 -0400

    Update Main.php

--- src/Dodgeball/Main.php
+++ src/Dodgeball/Main.php
@@ -26,50 +26,48 @@ class Main extends PluginBase implements Listener{
         $this->getLogger()->info(TextFormat::GREEN . "Dodgeball Minigame has been enabled successfully!");
     }
     
-      public function onJoin(PlayerJoinEvent $event) {
-    $player = $event->getPlayer();
-    $name = $event->getPlayer()->getName();
-    $player->sendMessage(TextFormat::AQUA . "> " . TextFormat::GREEN . "Welcome to " . TextFormat::YELLOW . TextFormat::BOLD ."StrongHold " . TextFormat::RESET . TextFormat::AQUA . $name . TextFormat::WHITE . "!");
-  }
-       
-   public function onHit(ProjectileHitEvent $event){
-       $entity = $event->getEntity();
-       if($player->getLevel()->getName() === "DodgeBall" && $player->getLevel()->getName() === "Kwa's Plot Spawn") {
-       $this->getLogger()->info(TextFormat::RED ."Someone has been eliminated in DodgeBall!");
-       $event->getServer()->getLevel()->broadcastMessage(TextFormat::AQUA . "> " . TextFormat::RED . $name . TextFormat::YELLOW . " has been eliminated in DodgeBall!");
-       $event->getEntity()->setHealth(0);
-   }
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $name = $event->getPlayer()->getName();
+        $player->sendMessage(TextFormat::AQUA . "> " . TextFormat::GREEN . "Welcome to " . TextFormat::YELLOW . TextFormat::BOLD ."StrongHold " . TextFormat::RESET . TextFormat::AQUA . $name . TextFormat::WHITE . "!");
+    }
     
-// onPlace
-    public function onPlace(BlockPlaceEvent $event) {
-   $player = $event->getPlayer();
-      if(!($player->isOp)) {
+    public function onHit(ProjectileHitEvent $event){
+        $entity = $event->getEntity();
         if($player->getLevel()->getName() === "DodgeBall" && $player->getLevel()->getName() === "Kwa's Plot Spawn") {
-          $event->setCancelled(true);
+            $this->getLogger()->info(TextFormat::RED ."Someone has been eliminated in DodgeBall!");
+            $event->getServer()->getLevel()->broadcastMessage(TextFormat::AQUA . "> " . TextFormat::RED . $name . TextFormat::YELLOW . " has been eliminated in DodgeBall!");
+            $event->getEntity()->setHealth(0);
         }
-
-// onBreak
-  public function onBreak(BlockBreakEvent $event) {
-      if(!($player->isOp)) {
-        if($player->getLevel()->getName() === "DodgeBall" && $player->getLevel()->getName() === "Kwa's Plot Spawn") {
-          $event->setCancelled(true);
-        }
-    
-       public function onDeath(PlayerDeathEvent $event){
-        $cause = $event->getEntity()->getLastDamageCause();
-        if($cause instanceof EntityDamageByEntityEvent){
-            $player = $event->getEntity();
-            $killer = $cause->getDamager();
-            $player = $event->getEntity();
-            if($killer instanceof Player){
-                $player->sendMessage(TextFormat::RED.$killer->getName() . TextFormat::RESET." §ekilled you with " .TextFormat::LIGHT_PURPLE.$killer->getHealth().TextFormat::GOLD." §chearts left" .TextFormat::YELLOW."!");
-                $killer->sendTIP(TextFormat::GREEN."You eliminated ".TextFormat::RED.$player->getName());
+        
+        public function onPlace(BlockPlaceEvent $event) {
+            $player = $event->getPlayer();
+            if(!($player->isOp)) {
+                if($player->getLevel()->getName() === "DodgeBall" && $player->getLevel()->getName() === "Kwa's Plot Spawn") {
+                    $event->setCancelled(true);
+                }
+                
+                public function onBreak(BlockBreakEvent $event) {
+                    if(!($player->isOp)) {
+                        if($player->getLevel()->getName() === "DodgeBall" && $player->getLevel()->getName() === "Kwa's Plot Spawn") {
+                            $event->setCancelled(true);
+                        }
+                        
+                        public function onDeath(PlayerDeathEvent $event){
+                            $cause = $event->getEntity()->getLastDamageCause();
+                            if($cause instanceof EntityDamageByEntityEvent){
+                                $player = $event->getEntity();
+                                $killer = $cause->getDamager();
+                                $player = $event->getEntity();
+                                if($killer instanceof Player){
+                                    $player->sendMessage(TextFormat::RED.$killer->getName() . TextFormat::RESET." §ekilled you with " .TextFormat::LIGHT_PURPLE.$killer->getHealth().TextFormat::GOLD." §chearts left" .TextFormat::YELLOW."!");
+                                    $killer->sendTIP(TextFormat::GREEN."You eliminated ".TextFormat::RED.$player->getName());
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-       }
-      }
-  }
-      }
     }
-   }
 }
